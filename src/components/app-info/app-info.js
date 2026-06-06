d578003f32d969c3e9f0e699b78956968c27186f
Roman <romansyplyviy@gmail.com>
2026-06-06 23:38:42 +0100

    feat: split app into components

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+function AppInfo() {
+  return (
+    <div className="app-info">
+      <h1>Облік співробітників в компанії NextServe</h1>
+      <h2>Загальна кількість співробітників: </h2>
+      <h2>Премію отримають: </h2>
+    </div>
+  );
+}
+
+export default AppInfo;
